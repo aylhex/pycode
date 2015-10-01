98670b633f693c0cc55204bed629c7520c8ac48a
nwcrazysword <nwcrazysword@gmail.com>
2015-10-02 00:13:34 +0800

    modify the method for myself

--- tools/identifycode/src/checkCharData.py
+++ tools/identifycode/src/checkCharData.py
@@ -1,12 +1,18 @@
 import json
+import sys
+import os
 
 def show(char):
-    with open('../json/%s.json'%char) as f:
+    fpath='../json_zhuhai/%s.json'%char
+    if not os.path.exists(fpath):
+        print 'The json file is not exist!'
+        return
+    with open(fpath) as f:
         dic = json.loads(f.read())
-        print dic
-        for i in xrange(dic['height']):
-            for j in xrange(dic['width']):
-                if [j,i] in dic['points']:
+        # print dic
+        for j in xrange(dic['height']):
+            for i in xrange(dic['width']):
+                if [i,j] in dic['points']:
                     print 'O',
                 else:
                     print ' ',
@@ -14,7 +20,11 @@ def show(char):
                     
 
 def main():
-    show('2')
+    if len(sys.argv)>1:
+        strcheck=sys.argv[1]
+        show(strcheck)
+    else:
+        show('2')
 
     
 if __name__ == '__main__':  
--- tools/identifycode/src/mergeData.py
+++ tools/identifycode/src/mergeData.py
@@ -1,65 +1,52 @@
-#coding=utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2015-10-01 23:55:38
+# @Author  : nwcrazysword (nwcrazysword@gmail.com)
+# @Link    : https://github.com/nwcrazysword
+# @Version : $Id$
+
 import json
 from checkCharData import show
 import pickle
+import os
 
-'''
-小写字母
-'''
-a = {}
-for i in xrange(26):
-    try:
-        c = chr(ord('a') + i)
-        f = open('../json/_%s.json'%c)
-        d = json.loads(f.read())
-        a[c] = d
-        f.close()
-    except:
-        pass
-    
-for char in a:
-    print '====== %s ======'%char
-    show('_'+char)
-    
-    
-    
-'''
-大写字母
-'''
-A = {}
-for i in xrange(26):
-    try:
-        c = chr(ord('A') + i)
-        f = open('../json/%s.json'%c)
-        d = json.loads(f.read())
-        A[c] = d
-        f.close()
-    except:
-        pass
-    
-for char in A:
-    print '====== %s ======'%char
-    #show(char)
-    
-    
-    
-'''
-数字
-'''
-N = {}
-for i in xrange(1,10):
-    try:
-        c = i
-        f = open('../json/%s.json'%c)
-        d = json.loads(f.read())
-        N[c] = d
-        f.close()
-    except:
-        pass
-    
-for char in N:
-    print '====== %s ======'%char
-    show(char)
+def GetTheJson(jsontype):
+    '''
+    jsontype:
+    low 小写字母
+    up  大写字母
+    num 数字
+    '''
+    dict_temp = {}
+    # path_json_pre='../json_zhuhai/'
+    if jsontype == 'low':
+        templist=xrange(26)
+        char_min='a'
+    if jsontype == 'up':
+        templist=xrange(26)
+        char_min='A'
+    if jsontype == 'num':
+        char_min='A'
+        templist=xrange(1,10)
+
+    for i in templist:
+        try:
+            if jsontype == 'num':
+                c=i
+            else:
+                c = chr(ord(char_min) + i)
+            fpath='../json_zhuhai/%s.json'%c
+            f = open(fpath)
+            d = json.loads(f.read())
+            dict_temp[c] = d
+            f.close()
+        except:
+            pass
+        
+    for char in dict_temp:
+        print '====== %s ======'%char
+        show('_'+char)
+        
 
 result = {}
 result.update(a)
@@ -70,6 +57,6 @@ for key in  result:
     result[key].pop('y_min')
     result[key].pop('x_max')
     result[key].pop('y_max')
-print result
+# print result
 with open('data.json','wb') as f:
     f.write(json.dumps(result))
