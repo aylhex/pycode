c97df7272e167fa6bafd52d5699570a9e10f936c
nwcrazysword <nwcrazysword@gmail.com>
2015-11-26 19:21:39 +0800

    add some method of arrow

--- tools/arrow/myarrow.py
+++ tools/arrow/myarrow.py
@@ -7,14 +7,18 @@
 
 import arrow
 
+truct="YYYY-MM-DD HH:mm:ss"
+
 def getmytime():
     # uttime=arrow.now()
+	string_res = '23333'
     uttime=arrow.utcnow()
     print uttime.timestamp
     print uttime.format('YYYY-MM-DD HH:mm:ss')
     print uttime.humanize()
     print uttime.weekday()
-
+	string_dist = arrow.get(string_res).format(truct)
+	string_dist = arrow.get(string_res).timestamp
 
 def main():
     getmytime()
